b097962e569f7ee06914e62721db1c5a50952ed1
심정민 <shjj09@naver.com>
2020-03-05 18:26:47 +0900

    코드 주석 추가

--- centras_back/routes/users.js
+++ centras_back/routes/users.js
@@ -1,32 +1,40 @@
 module.exports = (app) => {
   var express = require('express');
   var router = express.Router();
-  var mysql = require('mysql');
-  var dbOptions = require('./index.js');
+
+  // 암호화 library
   var crypto = require('crypto');
+
   var moment = require('moment');
-  require('moment-timezone');
+  require('moment-timezone');  
   moment.tz.setDefault("Asia/Seoul");
+
   require("dotenv").config();
+
+  // User 로그인 JWT Token 사용 
   const jwtSecret = process.env.JWT_SECRET;
   const jwt = require('jsonwebtoken');
   var isLogin = require('./token.js');
 
-  /* GET users listing. */
-
-  // url: user/login
-
   const conn = app.get('pool')
+
   
+  /*
+    유저 회원 가입   
+  */
+
   router.post('/sign_up', function (req,res) {
 
-    //pw 단방향 암호화 저장
+    //비밀번호 단방향 암호화 저장
 
     let inputPassword = req.body.pw;
     let salt = Math.round((new Date().valueOf() * Math.random())) + "";
     let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
 
     conn.query('SELECT * FROM user WHERE id = ?',[req.body.id],function (err,results) {
+
+      // 아이디 중복 확인 
+
       if(results[0])
         res.status(400).send({
           "message" : '중복된 아이디 입니다.'
@@ -59,10 +67,9 @@ module.exports = (app) => {
     });
   });
 
-
-  router.get('/login', function(req, res, next) {
-    res.render('users/login');
-  });
+  /*
+    유저 로그인   
+  */
 
   router.post('/login', function (req,res) {
 
@@ -70,11 +77,7 @@ module.exports = (app) => {
     let inputPassword = req.body.pw;
 
     conn.query('SELECT * FROM user WHERE id = ?',[id],function (err,results) {
-      if(!results[0])
-        res.status(400).send({
-          "message" : '중복된 아이디입니다.'
-        });
-      else {
+
         let user = results[0];
         let dbPassword = user.pw;
         let hashPassword = crypto.createHash("sha512").update(inputPassword + user.salt).digest("hex");
@@ -82,6 +85,7 @@ module.exports = (app) => {
         if(dbPassword == hashPassword){
           var date = moment().format('YYYY-MM-DD HH:mm:ss');
           conn.query('UPDATE user SET login_date=? WHERE id = ?',[date,user.id]);
+
           //token 발행
           let payload = {
             idx : user.idx,
@@ -90,14 +94,21 @@ module.exports = (app) => {
           };
           let option = {expiresIn : '1h'};
           token = jwt.sign(payload,jwtSecret,option);
-          res.status(200).send({'message':'로그인 성공','user':user,'token':token});
+
+          // token 값 출력
+          res.status(200).send({'message':'로그인 성공','token':token});
         }
         else{
           res.status(400).send({"message":'로그인 실패'});
         }
-      }
-    });
+      });
   });
+
+  /*
+    유저 개인정보 수정   
+  */
+
+  // isLogin 으로 token값 불러와서 user 정보 가져오기
 
   router.put('/',isLogin,function (req,res) {
     let user_idx = req.decoded.idx;
@@ -113,7 +124,6 @@ module.exports = (app) => {
       else{
         conn.query('UPDATE user SET id=?, pw=?, nickname=?, email=? WHERE idx = ?',[id,hashPassword,nickname,email,user_idx],function (err,results) {
           if (err) {
-                 console.log("error ocurred", err);
                  res.send.status(400)({
                      "message": "error ocurred"
                  });
