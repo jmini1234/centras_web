955e26a0f669e5ad2db0db2f0b1e8742bd0dcc45
심정민 <shjj09@naver.com>
2020-03-23 20:45:05 +0900

    centras_front merge

--- centras_front/src/App.js
+++ centras_front/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from "react-router-dom"; 
 import Router from './Routes/Router';
 import Header from './Layout/Header';
 import './App.css';
