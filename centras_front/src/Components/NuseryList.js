ff09c245bef63cb79cef8d0ac17b5969a7485c66
Jyw526 <55542627+Jyw526@users.noreply.github.com>
2020-04-23 14:11:43 +0900

    양식장 삭제 api 연결

--- centras_front/src/Components/NuseryList.js
+++ centras_front/src/Components/NuseryList.js
@@ -7,7 +7,37 @@ class NurseryList extends Component{
         isloggedIn : false 
         };
     }
+    handleDelete(e){
+        e.preventDefault();
+        console.log(e.target.value);
+        console.log(localStorage.getItem("AUTHORIZATION"));
+        const delete_info = {
+            method: "DELETE",
+            headers: {
+                'x-access-token' : localStorage.getItem("AUTHORIZATION"),
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        };
+        var idx = e.target.value;
+        console.log(idx);
+        fetch("http://localhost:3001/nursery/" + idx, delete_info)
+        .then(response => { 
+        console.log(response);
+        response.json().then(
+            result => {
+                console.log(result);
 
+                if(response.status == 400){
+                    alert("삭제 실패")
+                }
+                else{
+                    alert("삭제 성공")
+                    window.location.reload();
+                }
+            }
+        )
+        });
+    }
     componentWillMount(){
         const headers = {
             "x-access-token": localStorage.getItem("AUTHORIZATION"),
@@ -33,6 +63,7 @@ class NurseryList extends Component{
                 this.state.nursery_list.map((nursery)=>
                     <div>
                         {nursery.nursery_id}
+                        <button onClick = {this.handleDelete} value = {nursery.idx}>X</button>
                     </div>
                 )
         )
